6d72a6480f102e849ec3be81152b3aa1a9ecf50e
Agustin Linari <ALINARI@gire.com>
2019-09-06 00:52:19 -0300

    remove unused imports

--- src/main/java/com/geo/iptracker/config/ClientConfig.java
+++ src/main/java/com/geo/iptracker/config/ClientConfig.java
@@ -1,6 +1,5 @@
 package com.geo.iptracker.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/geo/iptracker/config/RedisConfig.java
+++ src/main/java/com/geo/iptracker/config/RedisConfig.java
@@ -1,9 +1,5 @@
 package com.geo.iptracker.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.geo.iptracker.repository.Stat;
 import com.geo.iptracker.repository.StatsRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -16,9 +12,7 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableRedisRepositories(basePackageClasses = StatsRepository.class)
